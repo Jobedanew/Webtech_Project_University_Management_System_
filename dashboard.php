9481731453551117fa4efdb6acbc1a4b13e4e8e0
Jobeda Hossain <jobedanew393@gmail.com>
2026-01-09 18:23:42 +0600

    header section edited

---
+++ dashboard.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container">
+
+    <div class="layout">
+
+    <!-- header section -->
+        <div class="header">
+             <a href="https://www.aiub.edu/">
+               <img src="aiub_logo.png" class="logo" alt="AIUB Logo">
+             </a>
+
+        </div>
+        
+       
+    </div>
+</div>
+
+
+</body>
+</html>
